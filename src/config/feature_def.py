0b00e64c0d386bcdf68ca8cf1bde7ea1c1e1e728
shawnjiang1019 <shawnj1019@gmail.com>
2025-12-01 16:11:29 -0500

    Add LSTM NN

--- src/config/feature_def.py
+++ src/config/feature_def.py
@@ -17,4 +17,16 @@ FEATURE_WITH_3M_COLS = [
     'y'
 ]
 
+FEATURE_NN_COLS = [
+    'NEIGHBOURHOOD_158',
+    'REPORT_YEAR',
+    'REPORT_MONTH',
+    'Prev_Month_NSI',
+    'NSI_3M_Avg',
+    'x',
+    'y',
+    'Crime_Count',
+    'TotalCrimeScore'
+]
+
 TARGET_COL = 'NSI'
